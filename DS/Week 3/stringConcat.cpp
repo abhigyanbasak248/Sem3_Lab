bca96ec640be99c6131cd908e698a0bca6522ba6
abhigyanbasak248 <96128313+abhigyanbasak248@users.noreply.github.com>
2022-11-27 19:41:28 +0530

    Add files via upload

---
+++ DS/Week 3/stringConcat.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+	string s1, s2;
+	cout << "Enter the first string  : ";
+	cin >> s1;
+	cout << "Enter the second string  : ";
+	cin >> s2;
+	string s3 = s1+s2;
+	cout << "Concatenated string  : " << s3;
+	
+}
